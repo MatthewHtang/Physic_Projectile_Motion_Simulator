4674d5188cd9e402db4f997d334884fdf987dd82
MatthewHtang <matthewhalingthang28@gmail.com>
2025-12-28 18:39:06 -0600

    added options for multiple planet gravities and fixed the output rounding

--- sim.py
+++ sim.py
@@ -7,6 +7,7 @@ from pylab import *
 initial_velocity = float(input ("Enter initial velocity (m/s): "))
 initial_height = float(input ("Enter initial height (m): "))
 launch_angle = float(input("Enter launch angle (degree): "))
+gravity = input("Enter your planet (earth, moon, mars, etc): ").lower()
 
 #converting angle
 launch_degree = math.radians(launch_angle)
@@ -16,30 +17,29 @@ vx = initial_velocity * math.cos(launch_degree)
 vy = initial_velocity * math.sin(launch_degree)
 
 #gravity
-earth_gravity = 9.8
-mercury_gravity = 3.7
-venus_gravity = 8.9
-moon_gravity = 1.6
-mars_gravity = 3.7
-jupiter_gravity = 23.1
-saturn_gravity = 9.0
-uranus_gravity = 8.7
-neptune = 11.0
-pluto = 0.7
+planet_gravity = [["earth", 9.8], ["mercury", 3.7], ["venus", 8.9], 
+                  ["moon", 1.6], ["mars", 3.7], ["jupiter", 23.1], 
+                   ["saturn", 9.0], ["uranus", 8.7], ["neptune", 11.0], 
+                   ["pluto", 0.7], ["sun", 274]]
 
+grav = None
+for planet in planet_gravity:
+    if planet [0] == gravity:
+        grav = planet[1]
 
-
+#if the initial height is 0 meter
 if initial_height <= 0:
-    time_travel = round((2* vy) / earth_gravity,2)
-    max_height = round((vy ** 2) / (2 * earth_gravity),2)
-    final_position = round(vx * time_travel,2)
-    
+    time_travel = (2 * vy) / grav
+    max_height = (vy ** 2) / (2 * grav)
+    final_position = vx * time_travel
+#if the initial height is greater than 0 meter
 elif initial_height != 0:
-    time_travel = round((vy+sqrt(vy**2 + 2*earth_gravity*initial_height))/earth_gravity,2)
-    max_height = round(initial_height + (vy**2)/(2*earth_gravity),2)
-    final_position = round(vx * time_travel,2)
+    time_travel = (vy + sqrt(vy**2 + 2 * grav * initial_height)) / grav
+    max_height = initial_height + (vy**2) / (2 * grav)
+    final_position = vx * time_travel
 
-print ("Time Travel ", time_travel )
-print ("Maximum Height ", max_height)
-print ("Final Position ", final_position)
+print("Time Travel: ", round(time_travel, 2), "s")
+print("Maximum Height: ", round(max_height, 2), "m")
+print("Final Position: ", round(final_position, 2),  "m")
+
 
